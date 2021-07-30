559677d5ff615b75ccead195e6657e6c85e3ce44
LeeSangIn <summacumlaude@naver.com>
2021-07-30 17:12:08 +0900

    LeeSangIn/Python

---
+++ 1.string/isomorphic_strings.py
@@ -0,0 +1,19 @@
+"""
+given two strings s and t, determine if they are Isomorphic.
+Isomorphic strings means all occurrences of a character must be replaced with another character,
+while preserving the order of characters.
+No two characters may map to the same character, but a character may map to itself.
+"""
+from lib import *
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    s1, s2 = "egg", "add"
+    print(s.isIsomorphic(s1, s2))
