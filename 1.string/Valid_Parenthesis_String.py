9459577274ea7ef9e2bb9bb08663e6fbd603720b
childult-programmer <63944028+childult-programmer@users.noreply.github.com>
2021-08-03 12:57:29 +0900

    LeeSangIn

--- 1.string/Valid_Parenthesis_String.py
+++ 1.string/Valid_Parenthesis_String.py
@@ -1,59 +1,24 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        left, asterisk = [], []  # each list contains '(' and '*'
 
-        # 빈 스택
-        stack = []
-        # 마지막 인덱스 변수
-        last_index = -1
-        # 문자열 s 순회
-        for i in range(0,len(s)):
-            if s[i] == '(':
-                stack.append('(')
-                last_index += 1
-
-            elif s[i] == '*':
-                stack.append('*')
-                last_index += 1
-
-            else:
-                if len(stack)==0:
+        for pos, char in enumerate(s):
+            if char == '(':
+                left.append(pos)
+            elif char == '*':
+                asterisk.append(pos)
+            else:  # char == ')'
+                if left:
+                    left.pop()
+                elif asterisk:
+                    asterisk.pop()
+                else:
                     return False
 
-                if stack[last_index] == '(':
-                    del stack[last_index]
-                    last_index -= 1
-
-                elif stack[last_index] == '*':
-                    # j 는 last_index 부터 0까지 돔
-                    flag =0
-                    for j in range(last_index,-1,-1):
-                        if stack[j]=='(':
-                            del stack[j]
-                            last_index -=1
-                            flag=1
-                            break
-                    if flag == 0 :
-                        del stack[last_index]
-                        last_index -= 1
-                else:
-                    print('스택에 ) 가 있는 경우는 없음')
-
-        s = stack
-        stack=[]
-        last_index = -1
-
-        for i in range(0,len(s)):
-            if s[i]=='(':
-                stack.append('(')
-                last_index +=1
-            else:
-                if last_index != -1 and stack[last_index]=='(':
-                    del stack[last_index]
-                    last_index -=1
-
-
-
-        for j in range(0,len(stack)):
-            if stack[j]=='(':
+        # pop until either list is empty
+        # '(' > '*' : False, '(' < '' : True
+        while left and asterisk:
+            if left.pop() > asterisk.pop():  # if left deque is empty
                 return False
-        return True
+
+        return len(left) == 0
