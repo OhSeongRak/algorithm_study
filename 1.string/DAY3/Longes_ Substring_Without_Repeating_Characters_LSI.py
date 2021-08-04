145fb6a8199399394dced73e2acc7cf7dcbd700c
childult-programmer <63944028+childult-programmer@users.noreply.github.com>
2021-08-04 14:54:41 +0900

    LeeSangIn

---
+++ 1.string/DAY3/Longes_ Substring_Without_Repeating_Characters_LSI.py
@@ -0,0 +1,28 @@
+"""
+- Brute Force => O(N^2)
+- Sliding Window => Two Pointer(not sorting) => O(N)
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        duplicate, left = {}, 0
+        max_length = 0
+
+        for right in range(len(s)):
+            if s[right] not in duplicate:
+                max_length = max(max_length, right - left + 1)
+            else:
+                if duplicate[s[right]] < left:
+                    max_length = max(max_length, right - left + 1)
+                else:
+                    left = duplicate[s[right]] + 1
+
+            duplicate[s[right]] = right
+
+        return max_length
+
+
+s = Solution()
+text = "tmmzuxt"
+print(s.lengthOfLongestSubstring(text))
