d640d436d50545b194937fbbe227bacd7a3ecd55
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2021-08-07 00:04:06 +0900

    Oh Seong Rak

---
+++ 1.string/DAY4/Permutation_in_String_OSR2.java
@@ -0,0 +1,41 @@
+package String;
+
+import java.util.Scanner;
+
+public class Permutation_in_String_OSR2 {
+
+	public static boolean checkInclusion(String s1, String s2) {
+		if (s1.length() > s2.length())
+			return false;
+
+		int cnt = s1.length();
+		int s = 0, e = 0;
+		int[] map = new int[26];
+		for (char c : s1.toCharArray())
+			map[c - 'a']++;
+
+		while (e < s2.length()) {
+			if (map[s2.charAt(e++) - 'a']-- > 0)
+				cnt--;
+			if (e - s == s1.length() && cnt == 0)
+				return true;
+			if (e - s == s1.length() && map[s2.charAt(s++) - 'a']++ >= 0)
+				cnt++;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String s1, s2;
+
+
+		Scanner in = new Scanner(System.in);
+
+		s1 = in.next();
+		s2 = in.next();
+
+		System.out.println(checkInclusion(s1, s2));
+	}
+
+}
