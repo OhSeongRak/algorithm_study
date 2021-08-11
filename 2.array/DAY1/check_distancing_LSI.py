588ac5b031d12ec9f3a7fd07ff59ac7c3436b26b
childult-programmer <63944028+childult-programmer@users.noreply.github.com>
2021-08-11 18:44:02 +0900

    LeeSangIn

--- 2.array/DAY1/check_distancing_LSI.py
+++ 2.array/DAY1/check_distancing_LSI.py
@@ -1,14 +1,12 @@
 from typing import *
 
 
-# 분할 상환 분석: 비관적
 def dfs(places: List[List[str]], i: int, j: int, manhattan: int = 0):
     # 재귀 종료: 1. 더 이상 동서남북 방향으로 이동할 수 없을 때, 2. 파티션('X')을 만났을 때, 3. Manhattan-Distance > 2
     if (i < 0 or i >= len(places) or j < 0 or j > len(places[0]) or
             places[i][j] == 'X' or manhattan > 2):
         return
 
-    # 거리두기 실패 : 맨해튼 거리 2 이하에서 'P'를 만났을 때,
     if places[i][j] == 'P' and manhattan <= 2:
         return False
 
