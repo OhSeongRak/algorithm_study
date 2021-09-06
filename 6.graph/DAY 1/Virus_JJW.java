cce1ac2a0d153ee030c45a0e78ef48c13a39fcdc
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2021-09-06 21:56:21 +0900

    Jeong Jiwon
    인접리스트로 변경

--- 6.graph/DAY 1/Virus_JJW.java
+++ 6.graph/DAY 1/Virus_JJW.java
@@ -6,7 +6,7 @@ public class Day1_Virus_JJW {
 	static int n,m;
 	static int worm_cnt = 0;
 	
-	public static void dfs(HashMap<Integer, List<Integer>> graph, int depth, boolean[] visited) {
+	public static void dfs(ArrayList<ArrayList<Integer>> graph, int depth, boolean[] visited) {
 		//재귀로 dfs
 		
 		visited[depth] = true; 
@@ -26,27 +26,29 @@ public class Day1_Virus_JJW {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer st;
+		StringTokenizer st ;
 		
 		
 		n = Integer.parseInt(br.readLine());	//정점
 		m = Integer.parseInt(br.readLine());	//간선
 		
 		
-		HashMap<Integer, List<Integer>> graph = new HashMap<>();
 		
-    	for(int i = 1 ; i < n+1 ; i++) {
-    		graph.put(i, new ArrayList<Integer>());
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		
+    	for(int i = 0 ; i < n+1 ; i++) {
+    		graph.add(new ArrayList<Integer>());
     	}
+    	
     	//System.out.println(graph);
+
     	boolean[] visited = new boolean[n+1];
     	
-		while(m-- > 0) {
-			st = new StringTokenizer(br.readLine());
+    	for(int i = 0 ; i < m; i++){
+			st = new StringTokenizer(br.readLine(), " ");
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			
-			//graph[start][end] = graph[end][start] = 1;
 			graph.get(u).add(v);
 			graph.get(v).add(u);
 		}
