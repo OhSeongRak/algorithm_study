005972d8a87e42c26835cabfbf0404a4dd0092f6
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2021-12-26 23:41:17 +0900

    Jeong Jiwon

--- 8.heap and priority queue/Car_Pooling.java
+++ 8.heap and priority queue/Car_Pooling.java
@@ -33,6 +33,36 @@ public class Day1_Car_Pooling {
 	     return true;
 	        
 	}
+	//Best Code
+	 public int nthSuperUglyNumber(int n, int[] primes) {
+        int i,j, size;
+        int[] index;
+        int[] ugly = new int[n];
+        
+        ugly[0] = 1; size = 1;
+        index = new int[ primes.length ];
+        for ( i=0; i<primes.length; i++ )
+        	index[i] = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>( 
+        		(e1,e2)->( primes[e1]*ugly[index[e1]] - primes[e2]*ugly[index[e2]] ) );
+        
+        for ( i=0; i<primes.length; i++ )
+        	queue.add( i );
+        while ( size<n )
+        {
+        	i = queue.poll();
+        	j = primes[i] * ugly[ index[i] ];
+        	if ( j>ugly[size-1] ) 
+        	{
+        		ugly[size] = j; 
+        		size++;
+        	}
+        	index[i]++;
+        	queue.add( i );
+        }
+    	return ugly[n-1];
+    }
+}
 	public static void main(String[] args) {
 		int[][] trips = { {9,3,4}, {9,1,7}, {4,2,4}, {7,4,5} };
 //		int[][] trips = { {3,2,7}, {3,7,9}, {8,3,9} };
