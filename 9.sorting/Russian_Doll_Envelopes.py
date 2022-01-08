5acd533d55c1bc907afc755ff9d07069275dbe59
childult-programmer <63944028+childult-programmer@users.noreply.github.com>
2022-01-09 03:58:05 +0900

    LeeSangIn

---
+++ 9.sorting/Russian_Doll_Envelopes.py
@@ -0,0 +1,17 @@
+from typing import List
+from bisect import bisect_left
+
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: [x[0], -x[1]])
+        lis = [envelopes[0][1]]
+
+        for _, height in envelopes:
+            if height > lis[-1]:
+                lis.append(height)
+            else:
+                index = bisect_left(lis, height)
+                lis[index] = height
+
+        return len(lis)
