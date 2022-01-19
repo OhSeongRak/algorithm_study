4f5ffedc77cf69108b2a87153c60165d42484868
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2022-01-19 15:48:54 +0900

    Create Longest Arithmetic Subsequence.py

---
+++ 10.Binary Search/DAY 2/Longest Arithmetic Subsequence.py
@@ -0,0 +1,11 @@
+from typing import *
+
+def longestArithSeqLength(A):
+    dp = {}
+    for start in range(len(A)):
+        for end in range(start + 1, len(A)):
+            dp[(end, A[end] - A[start])] = dp.get((start, A[end] - A[start]), 1) + 1
+    return max(dp.values())
+
+nums = [9,4,7,2,10]
+print(longestArithSeqLength(nums))
