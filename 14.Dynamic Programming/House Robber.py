fc4487d8e8447c676a4a989640700c182169087c
SeongRak Oh <81518305+OhSeongRak@users.noreply.github.com>
2022-02-10 18:59:53 +0900

    Oh Seong Rak

---
+++ 14.Dynamic Programming/House Robber.py
@@ -0,0 +1,28 @@
+from typing import *
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        maxList = []
+
+        maxList.append(nums[0])
+        maxList.append(max(nums[0], nums[1]))
+
+        for i in range(2, len(nums)):
+            maxList.append(max(maxList[i - 1], maxList[i - 2] + nums[i]))
+
+        return maxList[-1]
+
+sol = Solution()
+nums = [17, 11, 9, 4, 15, 12, 18, 8, 7, 8]
+
+print(sol.rob(nums))
+
+# import random
+#
+# list = []
+#
+# for i in range(10):
+#     list.append(random.randrange(1, 20))
+#
+# print(list)
